8e7c6dad9e07d062fd294e87b9eb0f902884b50e
Anthony Tleiji <anthonytleiji@gmail.com>
2023-11-11 20:22:46 +0200

    Island: - Add Mouse rotation handling - Add Keyboard rotation handling
    Sky:
    - Add Rotation
    
    Bird:
    - Add Animation
    
    Plane:
    - Add Animation

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -1,10 +1,37 @@
-import { useGLTF } from "@react-three/drei";
+import { useGLTF, useAnimations } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useEffect, useRef } from "react";
 import birdScene from "../assets/3d/bird.glb";
 
 const Bird = () => {
+  const ref = useRef();
   const { scene, animations } = useGLTF(birdScene);
+  const { actions } = useAnimations(animations, ref);
+
+  useFrame(({ clock, camera }) => {
+    ref.current.position.y = Math.sin(clock.elapsedTime) * 0.2 + 2;
+
+    if (ref.current.position.x > camera.position.x + 10) {
+      ref.current.rotation.y = Math.PI;
+    } else if (ref.current.position.x < camera.position.x - 10) {
+      ref.current.rotation.y = 0;
+    }
+
+    if (ref.current.rotation.y === 0) {
+      ref.current.position.x += 0.01;
+      ref.current.position.z -= 0.01;
+    } else {
+      ref.current.position.x -= 0.01;
+      ref.current.position.z += 0.01;
+    }
+  });
+
+  useEffect(() => {
+    actions["Take 001"].play();
+  }, []);
+
   return (
-    <mesh position={[-5, 2, 1]} scale={[0.003, 0.003, 0.003]}>
+    <mesh ref={ref} position={[-5, 2, 1]} scale={[0.003, 0.003, 0.003]}>
       <primitive object={scene} />
     </mesh>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
+import { useState } from "react";
 import Bird from "../models/Bird";
 import Island from "../models/Island";
 import Loader from "../components/Loader";
@@ -7,6 +8,9 @@ import Plane from "../models/Plane";
 import Sky from "../models/Sky";
 
 const Home = () => {
+  const [isRotating, setIsRotating] = useState(false);
+  const [currentStage, setCurrentStage] = useState(1);
+
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
     let screenPosition = [0, -6.5, -43];
@@ -19,8 +23,22 @@ const Home = () => {
     return [screenScale, screenPosition, islandRotation];
   };
 
+  const adjustPlaneForScreenSize = () => {
+    let screenScale, screenPosition;
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5];
+      screenPosition = [0, -1.5, 0];
+    } else {
+      screenScale = [3, 3, 3];
+      screenPosition = [0, -4, -4];
+    }
+    return [screenScale, screenPosition];
+  };
+
   const [islandScale, islandPosition, islandRotation] =
     adjustIslandForScreenSize();
+
+  const [planeScale, planePosition] = adjustPlaneForScreenSize();
 
   return (
     <section className="w-full h-screen relative">
@@ -28,7 +46,9 @@ const Home = () => {
         POPUP
       </div> */}
       <Canvas
-        className="w-full h-screen bg-transparent"
+        className={`w-full h-screen bg-transparent ${
+          isRotating ? "cursor-grabbing" : "cursor-grab"
+        }`}
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
@@ -40,13 +60,21 @@ const Home = () => {
             groundColor={"#000000"}
           />
           <Bird />
-          <Sky />
+          <Sky isRotating={isRotating} />
           <Island
             position={islandPosition}
             scale={islandScale}
             rotation={islandRotation}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
+            setCurrentStage={setCurrentStage}
           />
-          <Plane />
+          <Plane
+            position={planePosition}
+            scale={planeScale}
+            rotation={[0, 20, 0]}
+            isRotating={isRotating}
+          />
         </Suspense>
       </Canvas>
     </section>
